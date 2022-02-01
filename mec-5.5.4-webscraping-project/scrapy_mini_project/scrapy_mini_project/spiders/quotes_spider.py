b41ed1db29972aa0b7f8ae5661554378c08006d7
raywhelan01 <raywhelan01@gmail.com>
2022-02-01 00:39:19 -0800

    5.5.4 commit

--- mec-5.5.4-webscraping-project/scrapy_mini_project/scrapy_mini_project/spiders/quotes_spider.py
+++ mec-5.5.4-webscraping-project/scrapy_mini_project/scrapy_mini_project/spiders/quotes_spider.py
@@ -3,10 +3,13 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
-    ]
+
+    def start_requests(self):
+        url = 'http://quotes.toscrape.com/'
+        tag = getattr(self, 'tag', None)
+        if tag is not None:
+            url = url + 'tag/' + tag
+        yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
         for quote in response.css('div.quote'):
@@ -15,3 +18,30 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
             }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+
+class AuthorSpider(scrapy.Spider):
+    name = 'author'
+
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        author_page_links = response.css('.author + a')
+        yield from response.follow_all(author_page_links, self.parse_author)
+
+        pagination_links = response.css('li.next a')
+        yield from response.follow_all(pagination_links, self.parse)
+
+    def parse_author(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='').strip()
+
+        yield {
+            'name': extract_with_css('h3.author-title::text'),
+            'birthdate': extract_with_css('.author-born-date::text'),
+            'bio': extract_with_css('.author-description::text'),
+        }
